fc7b71046ab391c64aea2172a0644db2f8def71c
Diego Hiriart <hiriart.leon.d@gmail.com>
2022-12-09 13:56:45 -0500

    fix: call SpendFuel in Rocket's Fire implementation

--- Classes/Rocket.cs
+++ Classes/Rocket.cs
@@ -8,6 +8,7 @@ namespace Abstract_factory_Diego_Hiriart.Classes
             Console.WriteLine("Firing rocket");
             Console.WriteLine("Stats: \n\tHigh quality: {0} \n\tDamage: {1} hp \n\tBlast radius: {2} m \n\tFuel: {3} kg",
             this.HighQuality, this.Damage, this.BlastRadius, this.Fuel);
+            this.SpendFuel();
         }
     }
 }
